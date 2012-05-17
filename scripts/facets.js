50cf0ab929acdbac088969831e00d944938f8aad
Jim Balhoff <balhoff@nescent.org>
2012-05-16 22:20:58 -0400

    Facet paths working correctly.

--- scripts/facets.js
+++ scripts/facets.js
@@ -9,14 +9,27 @@ var facets = {
 		});	
 	},
 	
-	fill_facet: function (container, facet_key, facet_paths, compiled_query, callback) {
-		$(container).find(".facet-item").each(function (index, item) {
-			var iri = $(item).data("iri");
-			var params = facets.extract_query_parameters(iri, facet_key, facet_paths);
-			sparql.select(compiled_query(params), config.endpoint, function (sparql_json) {
-				partials.load_partial_each($(item).find(".facet-item-count"), "/shared/_count.html", sparql_json.results.bindings, callback);
+	fill_facet: function (container, facet_key, facet_paths, query_template, callback) {
+		partials.compile_template(query_template, function (compiled_query) {
+			var iris = _.toArray(_.map(facet_paths[facet_key], function (item) { return {iri: item, facet: facet_key};}));
+			iris.splice(0, 0, {iri: "any"});
+			partials.load_partial_each(container, "/facets/_facet_item.html", iris, function () {
+				var facet_items = $(container).find(".facet-item");
+				facet_items.each(function (index, item) {
+					var iri = $(item).data("iri");
+					if (iri == "any") {
+						iri = null;
+					}
+					var params = facets.extract_query_parameters(iri, facet_key, facet_paths);
+					var is_last = (index == (facet_items.size() - 1));
+					if (is_last) { $(item).addClass("focal-facet-item") };
+					sparql.select(compiled_query(params), config.endpoint, function (sparql_json) {
+						partials.load_partial_each($(item).find(".facet-item-count"), "/shared/_count.html", sparql_json.results.bindings, callback);
+					});
+					// start subclass queries here?
+				});
 			});
-		});
+		});	
 	},
 	
 	extract_query_parameters: function (iri, key, facet_paths) {
