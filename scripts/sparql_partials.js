d955886336dfaad9d50fe54c6513f5edbe93e422
Jim Balhoff <balhoff@nescent.org>
2013-02-05 16:53:58 -0600

    Added links to super/subclasses.

--- scripts/sparql_partials.js
+++ scripts/sparql_partials.js
@@ -1,12 +1,16 @@
 var sparql_partials = {
 	
-	load_partial_with_select: function (container, partial, query_template, endpoint, data) {
+	load_partial_with_select: function (container, partial, query_template, endpoint, data, content_for_empty) {
 		partials.render_template(query_template, data, function (query) { 
 			sparql.select(query, endpoint, function (sparql_json) {
-					var bindings_with_data = _.map(sparql_json.results.bindings, function (binding) {
-						return _.extend({}, data, binding);
-					});
-					partials.load_partial_each(container, partial, bindings_with_data);
+			        if (_.size(sparql_json.results.bindings) > 0) {
+			            var bindings_with_data = _.map(sparql_json.results.bindings, function (binding) {
+    						return _.extend({}, data, binding);
+    					});
+    					partials.load_partial_each(container, partial, bindings_with_data);
+			        } else {
+			            partials.load_partial(container, content_for_empty, data);
+			        }
 			}); 
 		});
 	}
